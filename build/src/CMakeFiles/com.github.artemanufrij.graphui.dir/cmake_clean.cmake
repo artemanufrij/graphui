1ae7232828c277a26264a43214cc6069e2705011
Artem Anufrij <artem.anufrij@live.de>
2018-02-12 17:33:33 +0100

    dev

---
+++ build/src/CMakeFiles/com.github.artemanufrij.graphui.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "Config.c"
+  "Services/GraphViz.c"
+  "MainWindow.c"
+  "Application.c"
+  "com.github.artemanufrij.graphui_valac.stamp"
+  "CMakeFiles/com.github.artemanufrij.graphui.dir/Config.c.o"
+  "CMakeFiles/com.github.artemanufrij.graphui.dir/Services/GraphViz.c.o"
+  "CMakeFiles/com.github.artemanufrij.graphui.dir/MainWindow.c.o"
+  "CMakeFiles/com.github.artemanufrij.graphui.dir/Application.c.o"
+  "com.github.artemanufrij.graphui.pdb"
+  "com.github.artemanufrij.graphui"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.artemanufrij.graphui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
